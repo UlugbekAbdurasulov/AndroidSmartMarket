0de4a281fec0f09372af40385095c360a1ae275d
Ulugbek <ulugbek352525@gmail.com>
2022-09-24 12:04:24 -0700

    SmartMarket4

--- app/src/main/java/com/example/androidsmartmarket/activity/main/category/CategoryFragment.kt
+++ app/src/main/java/com/example/androidsmartmarket/activity/main/category/CategoryFragment.kt
@@ -10,16 +10,51 @@ import com.example.androidsmartmarket.databinding.FragmentCategoryBinding
 import com.example.androidsmartmarket.databinding.FragmentHomeBinding
 
 
-class CategoryFragment: Fragment(R.layout.fragment_category) {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.core.app.ActivityOptionsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.androidsmartmarket.activity.DetailsActivity
+import com.example.androidsmartmarket.activity.viewmodel.SearchViewModel
+import com.example.androidsmartmarket.adabter.CategoryAdapter
+import com.example.androidsmartmarket.databinding.ActivitySearchBinding
+import com.example.androidsmartmarket.model.Datas
+import com.example.androidsmartmarket.model.Product
+import dagger.hilt.android.AndroidEntryPoint
 
-    private val binding by viewBinding(FragmentCategoryBinding::bind)
+@AndroidEntryPoint
+class CategoryFragment(function: (Long) -> Unit) : AppCompatActivity() {
+    private lateinit var binding: FragmentCategoryBinding
+    var categoryAdapter : CategoryAdapter? = null
+    val searchViewModel: SearchViewModel by viewModels()
+    var arrayList : ArrayList<Datas> = ArrayList()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentCategoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         initViews()
     }
 
     private fun initViews() {
+        binding.rvCategory.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+         categoryAdapter= CategoryAdapter {  }({ seletedItem: Long -> listItemClicked(seletedItem)})
+        binding.rvCategory.adapter = categoryAdapter
 
+    }
+
+    private fun listItemClicked(seletedItem: Long) {
+        searchViewModel.apiGetList(seletedItem)
+        searchViewModel.allPostsrter.observe(this,{
+            var intent = Intent(this, DetailsActivity::class.java)
+            intent.putExtra("datas", it.data)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this)
+            startActivity(intent,options.toBundle())
+            finish()
+        })
     }
 }
---
+++ app/src/main/java/com/example/androidsmartmarket/activity/viewmodel/CategoryViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.androidsmartmarket.activity.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.androidsmartmarket.activity.main.home.HomeFragment
+import com.example.androidsmartmarket.model.Technicals
+import com.example.androidsmartmarket.model.Welcome
+import com.example.androidsmartmarket.model.Welcomes
+import com.example.androidsmartmarket.network.service.PhotosService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryViewModel @Inject constructor(private val postService: PhotosService) : ViewModel(){
+    val allPosts = MutableLiveData<Welcome>()
+    val allPostsrter = MutableLiveData<Welcomes>()
+    fun apiSearchList(q : String) {
+        postService.searchProducts(q).enqueue(object : Callback<Welcome> {
+            override fun onResponse(call: Call<Welcome>, response: Response<Welcome>) {
+                allPosts.value = response.body()
+            }
+
+            override fun onFailure(call: Call<Welcome>, t: Throwable) {
+            }
+        })
+    }
+
+    fun apiGetList(id: Long) {
+            postService.listPhotosProduct(id,"ru",0,0).enqueue(object : Callback<Welcomes> {
+                override fun onFailure(call: Call<Welcomes>, t: Throwable) {
+                }
+
+                override fun onResponse(call: Call<Welcomes>, response: Response<Welcomes>) {
+                    allPostsrter.value = response.body()
+                }
+            })
+    }
+}
